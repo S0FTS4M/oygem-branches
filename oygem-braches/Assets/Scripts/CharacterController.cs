aa277795af817052d91780060b8aa0679a3e8fa8
softsam <sml.ozclk@gmail.com>
2021-09-04 21:03:40 +0300

    Update CharacterController.cs

--- oygem-braches/Assets/Scripts/CharacterController.cs
+++ oygem-braches/Assets/Scripts/CharacterController.cs
@@ -4,19 +4,8 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     private void Awake() {
         Debug.Log("Hello Oygem family");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
